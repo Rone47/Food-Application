fbbc7d564b7ebbd5c002fb28312c3150fba124da
Rone47 <ryanbyrone47@gmail.com>
2024-04-09 21:12:01 +0300

    Updates

--- src/forms/user-profile-form/manage-restaurant-form/ManageRestaurantForm.tsx
+++ src/forms/user-profile-form/manage-restaurant-form/ManageRestaurantForm.tsx
@@ -9,6 +9,8 @@ import Menusection from "./MenuSection";
 import ImageSection from "./ImageSection";
 import LoadingButton from "@/components/LoadingButton";
 import { Button } from "@/components/ui/button";
+import { useEffect } from "react";
+import { Restaurant } from "@/types";
 
 
 const formSchema = z.object({
@@ -45,11 +47,12 @@ const formSchema = z.object({
 type RestaurantFormData = z.infer<typeof formSchema>;
 
 type Props = {
+    restaurant?: Restaurant;
     onSave: (restaurantFormData: FormData) => void;
     isLoading: boolean;
 }
 
-const ManageRestaurantForm = ({ onSave, isLoading }: Props) => {
+const ManageRestaurantForm = ({ onSave, isLoading, restaurant }: Props) => {
     const form = useForm<RestaurantFormData>({
       resolver: zodResolver(formSchema),
       defaultValues: {
@@ -57,6 +60,30 @@ const ManageRestaurantForm = ({ onSave, isLoading }: Props) => {
         menuItems: [{ name: "", price: 0 }],
       },
     });
+
+    useEffect(() => {
+       if (!restaurant) {
+        return;
+       }
+
+       const deliveryPriceFormatted = parseInt (
+        (restaurant.deliveryPrice / 100).toFixed(2)
+       );
+
+       const menuItemsFormatted = restaurant.menuItems.map((item) => ({
+        ...item,
+        price: parseInt((item.price / 100).toFixed(2)),
+      }));
+
+      const updatedRestaurant = {
+        ...restaurant,
+        deliveryPrice: deliveryPriceFormatted,
+        menuItems: menuItemsFormatted,
+      };
+
+      form.reset(updatedRestaurant);
+       
+    }, [form, restaurant]);
 
     const onSubmit = (formDataJson: RestaurantFormData) => {
         const formData = new FormData();
@@ -86,9 +113,9 @@ const ManageRestaurantForm = ({ onSave, isLoading }: Props) => {
 
         if (formDataJson.imageFile) {
             formData.append(`imageFile`, formDataJson.imageFile);
-        }
-
-        onSave(formData);
+          }
+      
+          onSave(formData);
         
     }
 
--- src/pages/ManageRestaurantPage.tsx
+++ src/pages/ManageRestaurantPage.tsx
@@ -1,11 +1,14 @@
-import { useCreateMyRestaurant } from "@/api/MyRestaurantApi";
+import { useCreateMyRestaurant, useGetMyRestaurant } from "@/api/MyRestaurantApi";
 import ManageRestaurantForm from "@/forms/user-profile-form/manage-restaurant-form/ManageRestaurantForm";
 
 
 const ManageRestaurantPage = () => {
     const {createRestaurant, isLoading} = useCreateMyRestaurant();
+    const { restaurant } = useGetMyRestaurant();
+
     return (
         <ManageRestaurantForm
+          restaurant={restaurant}
           onSave={createRestaurant}
           isLoading={isLoading}
         />
